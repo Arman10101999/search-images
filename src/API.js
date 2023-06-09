4a1c9cc55a6667995a910bbc3d1eba65e019a838
Arman Srivastava <arman.bxm@gmail.com>
2023-06-09 12:13:41 +0530

    completing search images application

---
+++ src/API.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const SearchImage = async(term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID eXY_pJYwjEQLnCcOYNZ3We1764xnDY_pnJ8oWv4RLCs',
+        },
+        params: {
+            query: term,
+
+        }
+    });
+
+
+    return response.data.results;
+}
+
+export default SearchImage;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchImage from "./API";
+import "./App.css";
+import SeachBar from "./components/SeachBar";
+
+import { useState } from "react";
+import ImageList from "./components/ImageList";
 
 function App() {
+  const [images, setImages] = useState("");
+
+  const handleSubmit = async (term) => {
+    //do handling of the variable defined
+    const result = await SearchImage(term);
+
+    setImages(result);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SeachBar onSubmit={handleSubmit} />
+
+      <ImageList images={images} />
     </div>
   );
 }
